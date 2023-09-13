f0373bf79d293e45d6456ff8f0275908b8a73eb1
dimakohanskyi <dimakohanskyi@gmail.com>
2023-09-13 11:46:37 +0300

    Add new pages templates

--- app.py
+++ app.py
@@ -30,6 +30,24 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/product')
+def product_page():
+    return render_template('product.html')
+
+
+@app.route('/services')
+def services_page():
+    return render_template('services.html')
+
+
+@app.route('/contacts')
+def contacts_page():
+    return render_template('contacts.html')
+
+@app.route('/location')
+def location_page():
+    return render_template('location.html')
+
 @app.route('/register')
 def register_page():
     return render_template('register.html')
